71f9e7264ef553f21406c948be074a78c60d0ff0
David Bethune <david@mimix.io>
2021-04-29 10:39:12 -0500

    Walkthrough Version w/ Rom

--- ts/simple-greeting.ts
+++ ts/simple-greeting.ts
@@ -2,14 +2,14 @@ import {html, css, LitElement} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 
 
-@customElement('simple-greeting')
+@customElement('hello-mimix')
 export class SimpleGreeting extends LitElement {
-  static styles = css`p { color: blue }`;
+  static styles = css`p { color: #ec2028; font-family: Inter Black; font-size: 24pt }`;
 
-  @property()
-  name = 'Somebody';
+  @property() name = 'Somebody';
 
   render() {
     return html`<p>Hello, ${this.name}!</p>`;
+
   }
 }
