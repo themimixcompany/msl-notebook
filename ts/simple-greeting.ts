30cbd28b31ebb9c29dbbca58d008a9d41ffdd525
David Bethune <david@mimix.io>
2021-04-30 11:02:00 -0500

    MSL Notebook for 1st Screenshot

--- ts/simple-greeting.ts
+++ ts/simple-greeting.ts
@@ -4,12 +4,38 @@ import {customElement, property} from 'lit/decorators.js';
 
 @customElement('hello-mimix')
 export class SimpleGreeting extends LitElement {
-  static styles = css`p { color: #ec2028; font-family: Inter Black; font-size: 24pt }`;
+  static styles = css`
+    p, textarea { color: #ec2028; font-family: Inter Black; font-size: 24pt }
+    input { font-family: Inter; font-size: 24pt }
+    `;
 
-  @property() name = 'Somebody';
+  //Define public properties (databinding)
+  @property() name = 'World';
+  @property() mslResults = '';
 
+
+  //Something changed in the Name input box
+  nameBoxChanged(event: Event) {
+    this.name = (event.target as HTMLInputElement).value;
+  }
+
+  //Something changed in the MSL input box
+  mslBoxChanged(event: Event) {
+    const latestInput = (event.target as HTMLInputElement).value;
+    this.mslResults = `${this.mslResults}\n ${latestInput}`;
+    event.target.value = '';
+  }
+
+
+  //Show this component on screen
   render() {
-    return html`<p>Hello, ${this.name}!</p>`;
+    return html`
+    <input @change=${this.nameBoxChanged} placeholder="Your Name Here"><br>
+    <input @change=${this.mslBoxChanged} placeholder="(msl)"></input>
+    <p>Hello, ${this.name}!</p>
+    <textarea id="mslResultsBox" rows="20" cols="100">${this.mslResults}</textarea>
+    `;
 
   }
+
 }
